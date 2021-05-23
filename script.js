748c6351e9f4767f2bfe6f13dfa0b70ef0ca5967
chrisstoll1 <stoll101@gmail.com>
2021-05-23 17:01:41 -0400

    Initial Commit

---
+++ script.js
@@ -0,0 +1,134 @@
+const board = [];
+let colorFlag = false;
+let winner = false;
+
+function generateBoard() {
+    //42 boxes
+    var columnCounter = 0;
+    var i;
+    for (i = 0; i < 42; i++) { 
+        var box = {
+            column: columnCounter, //0-6
+            piece: false,
+            color: "",
+            winnerSquare: false
+        }
+
+        board.push(box);
+
+        columnCounter++
+        if (columnCounter === 7){
+            columnCounter = 0;
+        }
+    }
+    generateBoardHTML();
+}
+
+function generateBoardHTML() {
+    var boardRoot = document.getElementsByClassName("board-root")[0];
+    boardRoot.innerHTML = "";
+
+    board.forEach(box => {
+        // <div class="board-piece-box">
+        //     <div class="board-piece-red"></div>
+        // </div>
+        var boxObject = document.createElement("div");
+        boxObject.className = "board-piece-box";
+        if (box.winnerSquare){
+            boxObject.classList.add("winnerSquare");
+        }
+        boxObject.onclick = function() { columnClicked(`${box.column}`); };
+        if (box.piece){
+            boxObject.innerHTML = `<div class="board-piece-${box.color}"></div>`;
+        }
+        boardRoot.appendChild(boxObject);
+    });
+
+    var fireworks = document.getElementsByClassName("pyro")[0];
+    fireworks.style.display = "none";
+}
+
+function columnClicked(col) {
+    var colNum = parseInt(col);
+    var boxIndex = -1;
+    board.forEach(box => {
+        if (box.column === colNum && !box.piece){
+            var index = board.indexOf(box);
+            if (index > boxIndex){
+                boxIndex = index;
+            }
+        }
+    });
+    if (boxIndex > -1 && !winner){
+        board[boxIndex].piece = true;
+        if (colorFlag){
+            board[boxIndex].color = "yellow";
+            colorFlag = false;
+        }else{
+            board[boxIndex].color = "red";
+            colorFlag = true;
+        }
+    }
+    checkForWinner();
+    generateBoardHTML();
+}
+
+function checkForWinner() {
+    var checkArray = [-8, -7, -6, -1, 1, 6, 7, 8];
+    var redBoxes = [];
+    var yellowBoxes = [];
+
+    board.forEach(box => {
+        if (box.piece){
+            var index = board.indexOf(box);
+            if (box.color === "red"){
+                redBoxes.push(index);
+            }else{
+                yellowBoxes.push(index);
+            }
+        }
+    });
+
+    tallyPoints(redBoxes, checkArray);
+    tallyPoints(yellowBoxes, checkArray);
+}
+
+function tallyPoints(boxIndexes, checkArray){
+    boxIndexes.forEach(index => {
+        checkArray.forEach(direction => {
+            searchDirection(index, direction, 1, boxIndexes, []);
+        });
+    });
+}
+
+async function searchDirection(index, direction, temp, boxIndexes, searchIndexes){
+    searchIndexes.push(index);
+    var search = index + direction;
+    var p = temp;
+    if (boxIndexes.includes(search)){
+        p = p + 1;
+        await searchDirection(search, direction, p, boxIndexes, searchIndexes);
+        if (p === 4 && !winner){
+            console.log(`Winner! Points: ${p} Direction: ${direction} Search Indexes: ${searchIndexes}`);
+            winner = true;
+            win(searchIndexes);
+        }
+    }
+}
+
+function win(indexes){
+    indexes.forEach(index => {
+        board[index].winnerSquare = true;
+    });
+    generateBoardHTML();
+    var fireworks = document.getElementsByClassName("pyro")[0];
+    fireworks.style.display = "block";
+}
+
+function resetBoard(){
+    console.log("Resetting Board");
+    board.length = 0;
+    colorFlag = false;
+    winner = false;
+    generateBoard();
+}
